b08649dce6a3eec42f906db03a53cd059ec5fd33
Jorren Bosga <j.bosga@amsterdam.nl>
2021-06-17 23:30:46 +0000

    Fixes bug in setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="oistyle",
+    name="oistyle_python",
     version="0.0.1",
     author="Jorren Bosga",
     author_email="j.bosga@amsterdam.nl",
     description="Amsterdam OIS house style.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jbosga-ams/oistyle",
+    url="https://github.com/jbosga-ams/oistyle-python",
     project_urls={
-        "Bug Tracker": "https://github.com/jbosga-ams/oistyle/issues",
+        "Bug Tracker": "https://github.com/jbosga-ams/oistyle-python/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    data_files=[("etc/oistyle/", ["src/oistyle/base_style.json"])],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
